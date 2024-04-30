89857715b1bd4ea28c67151b1c09cc17b5f8ea29
Yuvraj Rajbhar <yuvraj.raj.842@gmail.com>
2024-04-30 22:47:55 +0530

    nav update

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,9 +1,9 @@
 
 import React, { useState } from 'react'
 import { Link, useNavigate } from "react-router-dom";
-import Badge from "@material-ui/core/Badge";
-import ShoppingCartIcon from "@material-ui/icons/ShoppingCart";
-import { useCart } from './ContextReducer';
+// import Badge from "@material-ui/core/Badge";
+// import ShoppingCartIcon from "@material-ui/icons/ShoppingCart";
+// import { useCart } from './ContextReducer';
 import Modal from '../Modal';
 import Cart from '../screens/Cart';
 export default function Navbar(props) {
